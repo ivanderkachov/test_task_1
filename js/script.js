0ea8e62b035d0d75d4863623e302e3d557036ce2
Деркачев Иван <derkachov_ivan@host-192-168-0-103.sohonet.ua>
2022-09-10 17:05:55 +0300

    added archive functionality

--- js/script.js
+++ js/script.js
@@ -16,7 +16,10 @@ const data = [
   },
 ]
 
+const dataArchive = []
+
 const element = document.querySelector(".table");
+const archive = document.querySelector(".archive")
 const input = document.querySelector(".input");
 const btn = document.querySelector(".input-button");
 
@@ -37,8 +40,13 @@ function delItem(index) {
 }
 function editItem(item) {
   data.splice(item.id, 1, item)
+  displayList();
 }
-
+function addToArchive(item) {
+  dataArchive.push(item)
+  console.log(dataArchive)
+  displayList();
+}
 
 btn.addEventListener('click', (e) => {
   e.preventDefault()
@@ -48,17 +56,29 @@ btn.addEventListener('click', (e) => {
 
 function displayList() {
   let task = ''
+  let taskArchive = ''
   console.log(data.length)
   if (data.length > 0) {
       data.forEach((user, index) => {
-        task += `<div attdiv="${index}" class="table_inside">${user.name} ${user.age}
-    <button attbutton="${index}" type="button" class="button__edit"> Edit </button>
-     <button attbutton="${index}" type="button" class="button"> Delete </button>
+        task += `<div name="${user.name}" age="${user.age}" attdiv="${index}" class="table_inside">${user.name} ${user.age}
+    <button name="${user.name}" age="${user.age}" attbutton="${index}" type="button" class="button__edit"> Edit </button>
+    <button name="${user.name}" age="${user.age}" attbutton="${index}" type="button" class="button"> Delete </button>
+    <button name="${user.name}" age="${user.age}" attbutton="${index}" type="button" class="button__archive">Archive</button>
     </div>`;
         element.innerHTML = task;
       });
   } else {
     element.innerHTML = "NONE";
+  }
+  if (dataArchive.length > 0) {
+    dataArchive.forEach((user, index) => {
+      taskArchive += `<div name="${user.name}" age="${user.age}" attdiv="${index}" class="table_inside">${user.name} ${user.age}
+    <button name="${user.name}" age="${user.age}" attbutton="${index}" type="button" class="button__edit"> Edit </button>
+    </div>`;
+      archive.innerHTML = taskArchive;
+    });
+  } else {
+    archive.innerHTML = "NONE";
   }
 
 }
@@ -73,17 +93,27 @@ element.addEventListener('click', function (e) {
   }
   if (e.target.closest('.button__edit')) {
     const attValue = e.target.closest(".button__edit").getAttribute("attbutton");
+    const editItem = e.target.closest(".button__edit").getAttribute("name");
     const editElement = document.querySelector(`[attdiv = "${attValue}"]`)
-    console.log(editElement)
-    const taskToEdit = `<input attinput="${attValue}" class="input__edit" type="text"/><button attbutton="${attValue}" class="button__save" type="button">Save</button>`
+    console.log(editItem, editElement)
+    const taskToEdit = `<input attinput="${attValue}" value="${editItem}" class="input__edit" type="text"/><button attbutton="${attValue}" class="button__save" type="button">Save</button>`
     editElement.innerHTML = taskToEdit
   }
   if (e.target.closest(".button__save")) {
     const inputEdit = document.querySelector('.input__edit')
     const attValue = e.target.closest(".button__save").getAttribute("attbutton")
     editItem({id: attValue, name: inputEdit.value, age: 5})
-    displayList()
     console.log("SAVE");
+  }
+  if (e.target.closest(".button__archive")) {
+    const attValue = e.target.closest(".button__archive").getAttribute("attbutton");
+    const archiveItem = {
+      id: e.target.closest(".button__archive").getAttribute("attbutton"),
+      name: e.target.closest(".button__archive").getAttribute("name"),
+      age: e.target.closest(".button__archive").getAttribute("age"),
+    };
+    delItem(attValue)
+    addToArchive(archiveItem)
   }
 })
 
